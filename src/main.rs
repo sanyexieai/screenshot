be0150b7e88835eecfb82dd30d00254cfda745d1
7mr <mail@7mr.net>
2024-10-28 18:23:57 +0800

    进程间通信（未完成）

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,145 @@
 use screenshot::impl_platform::{self, impl_hotkey_manager::HotkeyManager};
-use slint::{Image, LogicalPosition, ModelRc, SharedPixelBuffer, VecModel};
+use slint::{Image, LogicalPosition, ModelRc, SharedPixelBuffer, VecModel, Weak};
 use anyhow::Result;
 use xcap::{image::{DynamicImage, GenericImage, RgbaImage}, Monitor};
-use std::{sync::{Arc, Mutex}, thread, time::Duration};
+use std::{sync::{mpsc, Arc, Mutex}, thread, time::Duration};
 
 slint::include_modules!();
 
-fn main() {
-    let manager = HotkeyManager::new();
-    const MOD_CONTROL: u32 = 0x0002; // Ctrl 键修饰符
-    const VK_A: u32 = 0x41;           // A 键虚拟键码
+// 定义一个回调函数类型，要求实现 Send
+type Callback = Arc<Mutex<Box<dyn FnMut(Main) + Send>>>;
 
-    // 注册快捷键 Ctrl + A
-    manager.register_hotkey(1, MOD_CONTROL, VK_A, || {
-        println!("Control + A pressed!"); // 输出热键触发信息
+fn main() -> Result<()>{
+    let main = Main::new()?; 
+    // 创建一个 Arc<Mutex<i32>>，初始化值为 0
+    let app = Arc::new(Mutex::new(main.as_weak()));
+
+    // 创建一个回调函数，使用 Arc<Mutex<...>> 包装以支持跨线程调用
+    let callback: Callback = Arc::new(Mutex::new(Box::new(|main| {
+        let image_list: VecModel<ImageData> = VecModel::from(vec![]);
+        main.set_images(ModelRc::new(image_list));
+        // println!("Callback called with value: {}", value);
+    })));
+
+    // 创建一个线程
+    let app_clone = Arc::clone(&app);
+    let callback_clone = Arc::clone(&callback); // 克隆回调
+
+    let thread = thread::spawn(move || {
+        // 在线程中修改计数器
+        {
+            let mut window = app_clone.lock().unwrap().upgrade().unwrap(); // 获取锁
+            // 调用回调函数
+            let mut callback = callback_clone.lock().unwrap(); // 获取锁
+            callback(window); // 使用解引用操作符调用
+        }
     });
+    Ok(())
+}
 
-    println!("Listening for hotkeys... Press Ctrl + C to exit.");
+// fn main() -> Result<()> {
+//     let main = Main::new()?; 
+//     // let app_clone = main.as_weak();
+//     let app =  Arc::new(Mutex::new(main.as_weak()));
+//     let app_clone = Arc::clone(&app);
+//     // 创建一个共享的二维动态数组
+//     let shared_data: Arc<Mutex<Vec<Vec<i32>>>> = Arc::new(Mutex::new(Vec::new()));
+//     thread::spawn(move || {
+//         // 初始化 image_list
+//         let image_list = VecModel::from(vec![]);
+//         image_list.push(ImageData {
+//             x: 1,
+//             y: 1,
+//             width: 1,
+//             height: 1,
+//             image: Image::from_rgba8(SharedPixelBuffer::new(1, 1)),
+//         });
+//         // let app_clone = Arc::clone(&app);
+//         // let app_clone = app.clone();
+//         // let t = app_clone.upgrade().unwrap();
+//         // let app_clone = t.as_weak();
+//         const MOD_CONTROL: u32 = 0x0002; // Ctrl 键修饰符
+//         const VK_A: u32 = 0x41;           // A 键虚拟键码x
+//         // 注册快捷键 Ctrl + A
+//         HotkeyManager::register_hotkey(1, MOD_CONTROL, VK_A, move || {
+//             // app_clone_clone;
+//             // screenshot(app_clone).unwrap();
+//             // let app_clone = t.as_weak();
+//             // let image_list: VecModel<ImageData> = VecModel::from(vec![]);
+//             // tx.send(image_list).unwrap();
+//             // app_clone_clone.upgrade().unwrap().set_images(ModelRc::new(image_list));
+//             app_clone.lock().unwrap().upgrade().unwrap().set_images(ModelRc::new(image_list));
+//             println!("Control + A pressed!"); // 输出热键触发信息
+//         });
+//         // app_clone.lock().unwrap().upgrade().unwrap().set_images(ModelRc::new(image_list));
+//     });
+//     main.run()?;
+//     Ok(())
+// }
 
-    // 保持主线程活着
-    loop {
-        thread::sleep(Duration::from_secs(1)); // 每隔1秒保持主线程活着
-    }
+// fn main() -> Result<()> {
+//     let main = Main::new()?; 
+//     let desktop_size = get_desktop_size()?;
+//     print!("Desktop size: {} x {}\n", desktop_size.width, desktop_size.height);
+    
+//     // 设置窗口大小和位置
+//     main.window().set_size(desktop_size);
+//     main.window().set_position(LogicalPosition::new(0.0, 0.0));
+//     // 创建一个共享的二维动态数组
+//     let shared_data: Arc<Mutex<Vec<Vec<i32>>>> = Arc::new(Mutex::new(Vec::new()));
+//     let app_clone = main.as_weak();
+
+//     register_hotkey(shared_data.clone(), ||{
+//         app_clone.upgrade().unwrap().set_images(ModelRc::new(image_list));
+//         println!("Control + A pressed!");
+//     });
+//     main.run()?;
+//     Ok(())
+// }
+
+
+fn screenshot(app_clone: Weak<Main>) -> Result<()> {
+        let desktop_size = get_desktop_size()?;
+        // 创建一个线程安全的共享 merged_img 对象
+        let merged_img = Arc::new(Mutex::new(RgbaImage::new(desktop_size.width as u32, desktop_size.height as u32)));
+        let merged_img_clone = Arc::clone(&merged_img);
+        let monitors = Monitor::all().unwrap();
+        
+        // 初始化 image_list
+        let image_list: VecModel<ImageData> = VecModel::from(vec![]);
+
+        // 并行处理每个监视器的图像捕获
+        monitors.iter().for_each(|monitor| {
+            let image = monitor.capture_image().unwrap();
+            let width = monitor.width();
+            let height = monitor.height();
+            let data = image.as_raw();
+            
+            let mut buffer = SharedPixelBuffer::new(width, height);
+            buffer.make_mut_bytes().clone_from_slice(data); // 使用 `clone_from_slice` 复制像素数据
+        
+            // 生成图像数据
+            let image_data = Image::from_rgba8(buffer);
+
+            // // 加锁时只修改 merged_img_lock，而不是整个操作都加锁
+            // {
+            //     let mut merged_img_lock = merged_img_clone.lock().unwrap();
+            //     // 如果需要的话，可以在这里更新 merged_img
+            //     let dynamic_image = DynamicImage::ImageRgba8(RgbaImage::from_raw(width, height, data.to_vec()).unwrap());
+            //     _ = merged_img_lock.copy_from(&dynamic_image, monitor.x() as u32, monitor.y() as u32);
+            // }
+
+            image_list.push(ImageData {
+                x: monitor.x(),
+                y: monitor.y(),
+                width: width as i32,
+                height: height as i32,
+                image: image_data,
+            });
+
+            print!("Monitor: {} - {} - {} - {}\n", monitor.x(), monitor.y(), monitor.width(), monitor.height());
+        });
+        Ok(())
 }
 
 // fn main() -> Result<()> {
--- src/windows/impl_hotkey_manager.rs
+++ src/windows/impl_hotkey_manager.rs
@@ -1,76 +1,41 @@
 use std::collections::HashMap;
 use std::ptr;
-use std::thread;
-use std::time::Duration;
+use winapi::shared::windef::HWND;
 use winapi::um::winuser::{GetMessageW, MSG, WM_HOTKEY, RegisterHotKey, UnregisterHotKey};
-use std::{process};
-use std::sync::{Arc, Mutex};
+use std::sync::{Mutex};
 use crossbeam::channel::{bounded, Sender};
 
-type Hotkey = (u32, u32);
-type Callback = Box<dyn Fn() + Send + Sync>;
-
 pub struct HotkeyManager {
-    callbacks: Arc<Mutex<HashMap<Hotkey, Callback>>>,
     exit_sender: Sender<()>,
 }
 
 impl HotkeyManager {
-    pub fn new() -> Self {
-        let (exit_sender, exit_receiver) = bounded::<()>(1);
-        let callbacks: Arc<Mutex<HashMap<Hotkey, Callback>>> = Arc::new(Mutex::new(HashMap::new()));
-        let cb_clone = Arc::clone(&callbacks);
-
-        // 启动消息监听线程
-        thread::spawn(move || {
-            let mut msg: MSG = unsafe { std::mem::zeroed() };
-
-            while unsafe { GetMessageW(&mut msg, ptr::null_mut(), 0, 0) } > 0 {
-                // 检查退出信号
-                if exit_receiver.try_recv().is_ok() {
-                    break;
-                }
-
-                if msg.message == WM_HOTKEY {
-                    let hotkey = (
-                        (msg.lParam as u32) & 0xFFFF,
-                        ((msg.lParam as u32) >> 16) & 0xFFFF,
-                    );
-
-                    println!("Hotkey received: {:?}", hotkey); // 输出热键信息
-                    if let Some(callback) = cb_clone.lock().unwrap().get(&hotkey) {
-                        callback();
-                    } else {
-                        println!("No callback found for: {:?}", hotkey); // 没有找到回调
-                    }
-                }
-            }
-
-            unsafe {
-                for id in 1..=cb_clone.lock().unwrap().len() as i32 {
-                    UnregisterHotKey(ptr::null_mut(), id);
-                }
-            }
-            process::exit(0);
-        });
-
-        Self { callbacks, exit_sender }
-    }
-
-    pub fn register_hotkey<F>(&self, id: u32, modifiers: u32, vk: u32, callback: F)
+    pub fn register_hotkey<F>( id: u32, modifiers: u32, vk: u32, callback: F)
     where
         F: Fn() + Send + Sync + 'static,
     {
-        let hotkey = (modifiers, vk);
-        unsafe {
-            if RegisterHotKey(ptr::null_mut(), id as i32, modifiers, vk) == 0 {
-                eprintln!("Failed to register hotkey: {:?} + {:?}", modifiers, vk);
-                return;
-            } else {
-                println!("Successfully registered hotkey: {:?} + {:?}", modifiers, vk);
+        let hwnd: HWND = std::ptr::null_mut();
+        let result: i32 = unsafe { RegisterHotKey(hwnd, id as i32, modifiers, vk) };
+        if result == 0 {
+            println!("Failed to register hotkey.");
+            return;
+        }
+        loop {
+            let mut msg = unsafe { std::mem::zeroed() };
+            let result = unsafe { winapi::um::winuser::GetMessageW(&mut msg, hwnd, 0, 0) };
+            if result == -1 {
+                println!("Failed to get message.");
+                break;
+            }
+            if msg.message == WM_HOTKEY {
+                let hotkey = (
+                    (msg.lParam as u32) & 0xFFFF,
+                    ((msg.lParam as u32) >> 16) & 0xFFFF,
+                );
+                println!("Hotkey received: {:?}", hotkey); // 输出热键信息
+                callback();
             }
         }
-        self.callbacks.lock().unwrap().insert(hotkey, Box::new(callback));
     }
 
     pub fn stop_listening(&self) {
